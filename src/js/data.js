f2e83783b19fcf5254fae08326b68aaf153107d0
xin00000lin <166885538+xin00000lin@users.noreply.github.com>
2024-04-14 22:18:48 -0400

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -20,4 +20,4 @@ let dataSetVersion = '';
 /**
  * Image root, will be appended to the start of every image URL.
  */
-const imageRoot = 'src/assets/chars';
+const imageRoot = 'https://i.imgur.com/';
